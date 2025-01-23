c843828d8a04c1f9ad98e4bcce90c9469c15ea33
John Hiner <johnh@respage.com>
2025-01-23 11:03:22 -0500

    Revert "Merge branch 'master-stg'"
    This reverts commit 279e9ce313b4d76bb1af3ed49ee0bef07f1b3a39, reversing
    changes made to b3e996ed44e41773f11c2200b729890dd32b0f77.

--- apps/unified-chatbot-api/src/services/google.service.ts
+++ apps/unified-chatbot-api/src/services/google.service.ts
@@ -1,19 +1,19 @@
-import {Logger} from "winston";
+import winston, {Logger} from "winston";
 import {forwardRef, Inject, Injectable} from '@nestjs/common';
 
-import {v2} from '@google-cloud/speech';
+import {SpeechClient} from '@google-cloud/speech';
 import {Duplex, TransformCallback} from "stream";
 import {VoiceService} from "./voice.service";
 import {ActiveCall, DONE_BUFFER} from "../models/active-call.model";
 import {TextToSpeechClient} from "@google-cloud/text-to-speech";
-import {google as googleTextToSpeech} from "@google-cloud/text-to-speech/build/protos/protos";
-import {google as googleSpeechToText} from "@google-cloud/speech/build/protos/protos";
+import {google} from "@google-cloud/text-to-speech/build/protos/protos";
+import AudioEncoding = google.cloud.texttospeech.v1.AudioEncoding;
 import {OpenAiService} from "./open-ai.service";
 import {WINSTON_MODULE_PROVIDER} from "nest-winston";
 
 @Injectable()
 export class GoogleService {
-    private speechToTextClient: v2.SpeechClient;
+    private speechToTextClient: SpeechClient;
     private textToSpeechClient: TextToSpeechClient;
 
     constructor(@Inject(forwardRef(() => VoiceService)) private voiceService: VoiceService,
@@ -21,7 +21,7 @@ export class GoogleService {
                 @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger) {
         const credentials = JSON.parse(Buffer.from(process.env.GOOGLE_CLOUD_CREDENTIALS, 'base64').toString('utf-8'));
         logger.info("GOOGLE CREDENTIALS", credentials);
-        this.speechToTextClient = new v2.SpeechClient({...credentials, apiEndpoint: 'https://us-central1-speech.googleapis.com'});
+        this.speechToTextClient = new SpeechClient({credentials});
         this.textToSpeechClient = new TextToSpeechClient({credentials})
     }
 
@@ -40,8 +40,14 @@ export class GoogleService {
                         call.startTyping().then();
 
                         const [result] = await original_this.speechToTextClient.recognize({
-                            content: new Uint8Array(Buffer.concat(chunks)),
-                            recognizer: 'projects/respage-app/locations/us-central1/recognizers/respage-unified-chatbot-chirp-phone-call-recognizer',
+                            audio: {content: Buffer.concat(chunks)},
+                            config: {
+                                encoding: 'LINEAR16',
+                                sampleRateHertz: 16000,
+                                languageCode: 'en-US',
+                                useEnhanced: true,
+                                model: 'phone_call',
+                            },
                         });
                         chunks = [];
                         const transcript = result?.results
@@ -55,7 +61,7 @@ export class GoogleService {
                             call.promptAI("Apologize, say you didn't catch that, and ask them to repeat themselves. Suggest they try using more words.");
                         }
                     } catch (e) {
-                        original_this.logger.error('Google speech to text stream: failed to recognize', {e});
+                        original_this.logger.error(e);
                         call.promptAI("Apologize because something has gone wrong and ask the user to try again.");
                         callback();
                         return;
@@ -153,7 +159,7 @@ export class GoogleService {
                     input: {text},
                     voice: {languageCode: 'en-US', ssmlGender: 'NEUTRAL'},
                     audioConfig: {
-                        audioEncoding: googleTextToSpeech.cloud.texttospeech.v1.AudioEncoding.LINEAR16,
+                        audioEncoding: AudioEncoding.LINEAR16,
                         sampleRateHertz: 16000
                     }
                 });
