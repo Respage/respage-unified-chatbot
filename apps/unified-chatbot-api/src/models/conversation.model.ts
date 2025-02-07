1175a68d761b65b337a0e3f77e23b4e05a6b7834
John Hiner <johnh@respage.com>
2025-02-07 16:05:24 -0500

    Wait 1 exchange before asking for name and SMS consent

--- apps/unified-chatbot-api/src/models/conversation.model.ts
+++ apps/unified-chatbot-api/src/models/conversation.model.ts
@@ -63,6 +63,7 @@ export class Conversation {
     propertyInfo: PropertyInfo;
     conversationInfo: ConversationInfo;
     timezone: string;
+    user_replied: boolean;
 
     private systemPrompt: ChatCompletionMessageParam;
 
