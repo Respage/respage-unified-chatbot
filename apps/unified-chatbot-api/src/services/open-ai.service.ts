5ae21fa7f82a5c0fa3ff2afeb6628afdff368a0a
John Hiner <johnh@respage.com>
2025-02-18 17:22:54 -0500

    Another fix

--- apps/unified-chatbot-api/src/services/open-ai.service.ts
+++ apps/unified-chatbot-api/src/services/open-ai.service.ts
@@ -284,6 +284,19 @@ export class OpenAiService {
                 if (tourDateTime) {
                     if ((smsConsent === true || smsConsent === false) && tourConfirmed) {
                         try {
+                            if (!call.conversation.conversationInfo.prospect._id) {
+                                const upsert = {
+                                    ...call.conversation.conversationInfo.prospect,
+                                    locale: 'en-US',
+                                    attribution_type: 'voice',
+                                    attribution_value: 'voice',
+                                    await_external_integration_ids: true
+                                };
+
+                                const newProspect = await this.resmateService.upsertProspect(call.conversation.campaign_id, upsert);
+                                call.updateSystemPrompt(null, await this.resmateService.mapExistingProspectInfo(newProspect));
+                            }
+
                             if (smsConsent) {
                                 await this.resmateService.upsertProspect(
                                     call.conversation.campaign_id,
@@ -302,19 +315,6 @@ export class OpenAiService {
                                 });
 
                             if (call.checkTourTimeAvailable(tourDateTime)) {
-                                if (!call.conversation.conversationInfo.prospect._id) {
-                                    const upsert = {
-                                        ...call.conversation.conversationInfo.prospect,
-                                        locale: 'en-US',
-                                        attribution_type: 'voice',
-                                        attribution_value: 'voice',
-                                        await_external_integration_ids: true
-                                    };
-
-                                    const newProspect = await this.resmateService.upsertProspect(call.conversation.campaign_id, upsert);
-                                    call.updateSystemPrompt(null, await this.resmateService.mapExistingProspectInfo(newProspect));
-                                }
-
                                 await this.resmateService.scheduleTour(call);
 
                                 call.updateSystemPrompt(null, {tour_scheduled: true});
